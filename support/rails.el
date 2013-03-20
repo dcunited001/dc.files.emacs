04e3510237071147fc39089ea8d5f3ea218d4ea7
David Conner <dconner.pro@gmail.com>
2013-03-20 04:52:17 -0600

    added Guardfile/Procfile to auto-ruby-mode

--- support/rails.el
+++ support/rails.el
@@ -24,6 +24,8 @@
 ;;==============================;;
 
 ;; syntax highlight Gemfile, etc.
+(add-to-list 'auto-mode-alist '("/Procfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("/Guardfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("/Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("/Vagrantfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("/Rakefile\\'" . ruby-mode))
