7e5bfd93f3c80375e0652b915c6f7dcd0f830875
David Conner <dconner.pro@gmail.com>
2013-06-17 17:46:43 -0600

    node.el - set js mode indent level

---
+++ support/node.el
@@ -0,0 +1,34 @@
+;; rails.el --- Rails on Emacs
+;;
+;; Copyright (c) 2013 David Conner
+;;
+;; Author: David Conner <dconner.pro@gmail.com>
+;; URL:
+;; Version: 0.0.1
+;; Keywords: me
+
+;;; Commentary:
+
+;; Commentary pour que!
+
+;;; License:
+
+;; ...
+
+;;; Code:
+
+;; ...
+
+;;==============================;;
+;; Node
+;;==============================;;
+
+;;==============================;;
+;; Javascript
+;;==============================;;
+(setq js-indent-level 2)
+
+;;==============================;;
+;; Coffeescript
+;;==============================;;
+
