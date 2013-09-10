5c45d3158210325ae107a3d1da3bb30eb92e0493
David Conner <dconner.pro@gmail.com>
2013-09-09 23:27:21 -0600

    add auto-complete config

---
+++ support/auto-complete.el
@@ -0,0 +1,10 @@
+;;==============================;;
+;; AUTO-COMPLETE
+;;==============================;;
+
+;; add auto-complete to completion-at-point-functions
+(defun set-auto-complete-as-completion-at-point-function ()
+    (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+
