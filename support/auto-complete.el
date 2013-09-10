6751bad25f41a9cc61f3843df41efe62aeb5c743
David Conner <dconner.pro@gmail.com>
2013-09-10 06:03:09 -0600

    auto-complete: formatting

--- support/auto-complete.el
+++ support/auto-complete.el
@@ -5,6 +5,8 @@
 ;; add auto-complete to completion-at-point-functions
 (defun set-auto-complete-as-completion-at-point-function ()
     (setq completion-at-point-functions '(auto-complete)))
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(add-hook 'auto-complete-mode-hook 
+          'set-auto-complete-as-completion-at-point-function)
 
 
