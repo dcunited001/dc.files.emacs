9516450a4a899926ee9f88e490175d0f95bc79f3
David Conner <dconner.pro@gmail.com>
2015-12-20 19:38:07 -0500

    using purcell/emacs.d.
    * mostly scrapping my own emacs dotfiles, will restructure soon
    
    * adding init-local.el, which is linked into ~/.emacs.d/lisp/

---
+++ init-local.el
@@ -0,0 +1,17 @@
+;; emacs config extensions
+;; - linked and loaded from ~/.emacs.d/lisp/init-local.el
+;; - i mostly gave up on writing my own emacs configs
+;; - they were far too messy, non-standard and incomplete
+;; - just cloned purcell's as a starting point https://github.com/purcell/emacs.d
+
+;; themes
+;; - cyberpunk
+;; - sanityink-tomorrow
+;; - zenburn
+;; - ample
+;; - ample-zen
+;; - noctilux
+
+(setq default-tab-width 2)
+
+(provide 'init-local)
