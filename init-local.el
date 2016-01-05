ae339577d0734867ae61640e7eaac9258c753532
David Conner <dconner.pro@gmail.com>
2016-01-05 17:48:05 -0500

    tried autoloading synonyms mode in markdown-mode
    * but there's a lot of wierd affects with this
    
    * also tried icicles, but didn't seem to jive with my emacs config

--- init-local.el
+++ init-local.el
@@ -18,29 +18,43 @@
 
 ;; TODO: config init files
 ;; (add-to-list 'load-path (expand-file-name "~/.files/emacs/support"))
-;; (require 'config-markdown)
+;; (require 'config-Markdown)
 
-;; Markdown ============
+;; ==========================================
+;; Load libs
+;; ==========================================
+
+;;(require-package 'icicles)
 
 ;; synosaurus requires wn command line tool
 ;;(require-package 'synosaurus)
+
 ;; http://www.emacswiki.org/emacs/Synonyms#toc3
 (setq synonyms-file "~/.files/mthesaur.txt")
 (setq synonyms-cache-file "~/.files/mthesaur.cache")
 (require-package 'synonyms)
 
-(defun setup-markdown-mode
-    ;;(synosaurus-mode)
-    (synonyms-mode))
+;; ==========================================
+;; markdown Mode
+;; ==========================================
 
-(add-hook 'markdown-mode
-          'setup-markdown-mode)
+;; NOTE: can't figure out how to load synonyms-mode
+;; - without overriding markdown mode (since it's a major mode)
+;; - and without screwing up the undo data for the buffer
+;; - and for some reason `synonyms` command is not available unless major mode is loaded
+
+;; synosaurus mode requires sn cmd line tool
+;; icicles mode needs to load before synonyms
+;; (defun setup-markdown-mode
+;;(icy-mode)
+;; (synonyms-mode))
+
+;; (add-hook 'markdown-mode-hook
+;; 'setup-markdown-mode)
 
 ;; (autoload 'markdown-mode "markdown-mode"
 ;; "Major mode for editing Markdown files" t)
 ;;(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-;; Markdown ============
 
 (provide 'init-local)
 
