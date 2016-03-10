8e116030dcf2a0ba6da8dcafe5e499161ebde693
David Conner <dconner.pro@gmail.com>
2016-03-10 17:21:21 -0500

    swift: add swift-mode and add swift to flycheck

---
+++ support/config-swift.el
@@ -0,0 +1,11 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+(require-package 'swift-mode)
+(add-to-list 'flycheck-checkers 'swift)
+
+(provide 'config-swift)
+
+;;; config-ruby.el ends here
