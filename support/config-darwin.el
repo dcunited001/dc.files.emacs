27eb07521932616975bf2f9afe230fbb4f555c2f
David Conner <dconner.pro@gmail.com>
2019-09-11 05:49:53 -0400

    reverting to a more default MacOS modkey config
    * running into conflicts with some of the osx mission ctrl keys

--- support/config-darwin.el
+++ support/config-darwin.el
@@ -10,11 +10,12 @@
 ;; font size
 (set-frame-font "DejaVu Sans Mono for Powerline" 14 t)
 
-(setq ns-control-modifier 'super)
+(setq ns-control-modifier 'alt)
 (setq ns-right-control-modifier 'control)
-(setq ns-command-modifier 'control)
+(setq ns-command-modifier 'super)
+(setq ns-right-command-modifier 'control)
 (setq ns-alternate-modifier 'meta)
-(setq ns-right-alternate-modifier 'alt)
+(setq ns-right-alternate-modifier 'meta)
 
 (provide 'config-darwin)
 
